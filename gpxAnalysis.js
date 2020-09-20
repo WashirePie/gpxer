30bac95861dd9ebcea971b990cd39ac8db40ec49
Simon Schödler <simon.schoedler@hotmail.ch>
2020-09-20 23:48:37 +0200

    Added dat.gui

--- gpxAnalysis.js
+++ gpxAnalysis.js
@@ -1,20 +1,31 @@
 class GPXHikeAnalysis
 {
-    /* w = hiker weigh [kg], wbp = backpackweight [kg], tour = GPXTrack || GPXRoute */
-    constructor(w, wbp, tour) 
+    /* w = hiker weigh [kg], aw = additional weight (ex. Backpack) [kg], tour = GPXTrack || GPXRoute */
+    constructor(w, aw, tour) 
     {
-        if (!(tour instanceof GPXRoute || tour instanceof GPXTrack)) { GPXConverter.e(`'tour' must be instance of 'GPXTrack' or 'GPXRoute', but it's '${tour.consstructor.name}'!`); return; }
+        if (!(tour instanceof GPXRoute || tour instanceof GPXTrack)) { GPXConverter.e(`'tour' must be instance of 'GPXTrack' or 'GPXRoute', but it's '${tour.constructor.name}'!`); return; }
         if (!(tour.content.every(o => o.attributes.hasOwnProperty('ele')))) { GPXConverter.e(`insufficient data! Not every element in 'tour.content' has a 'ele' property!`); return; }
         if (!(tour.content.every(o => o.attributes.hasOwnProperty('time')))) { GPXConverter.e(`insufficient data! Not every element in 'tour.content' has a 'time' property!`); return; }
 
         this.hikerWeight = w;
-        this.totalWeight = w + wbp;
+        this.additionalWeight = aw;
         this.tour = tour;
+        
+        /* Chart js data */
+        this.chartLabels = [];
+        this.chartDatasets = [];
     }
 
     analyze = () =>
     {
         let kcal = 0;
+
+        let elevationDataset = new ChartDataset('Elevation [m]', 'rgba(0,0,0,0)', 'rgba(200,0,0,1)');
+        let energyDataset = new ChartDataset('Energy [kcal]', 'rgba(0,0,0,0)', 'rgba(0,200,0,1)');
+        let speedDataset = new ChartDataset('Pace [m/s]', 'rgba(0,0,0,0)', 'rgba(0,0,200,1)');
+        
+        this.chartLabels = [];
+        this.chartDatasets = [];
 
         for (let i = 0; i < this.tour.content.length - 1; i++)
         {
@@ -33,19 +44,36 @@ class GPXHikeAnalysis
                 ee /= 60 * 60;
                 ee *= Δt;
 
-                let e = GPXHikeAnalysis.wattPerKilogramToKcal(ee) * this.totalWeight;
-
-                console.log(`Op[${i}]:`);
-                console.log(`          d     = ${d} meters`);
-                console.log(`          t     = ${Δt} seconds`);
-                console.log(`          Δh    = ${Δh} meters`);
-                console.log(`          v     = ${v} m/s`);
-                console.log(`          slope = ${ii} %`);
-                console.log(`          e     = ${e} kcal`);
-
+                let e = GPXHikeAnalysis.wattPerKilogramToKcal(ee) * (this.hikerWeight + this.additionalWeight);
+                
                 kcal += e;
+
+                /* Log Data */
+                // console.log(`Op[${i}]:`);
+                // console.log(`          d     = ${d} meters`);
+                // console.log(`          t     = ${Δt} seconds`);
+                // console.log(`          Δh    = ${Δh} meters`);
+                // console.log(`          v     = ${v} m/s`);
+                // console.log(`          slope = ${ii} %`);
+                // console.log(`          e     = ${e} kcal`);
+
+                /* Collect Chart data */
+                let timeObj = this.tour.content[i + 1].attributes.time;
+                let hours = timeObj.getHours() < 10 ? `0${timeObj.getHours()}` : `${timeObj.getHours()}`;
+                let mins = timeObj.getMinutes() < 10 ? `0${timeObj.getMinutes()}` : `${timeObj.getMinutes()}`;
+                let secs = timeObj.getSeconds() < 10 ? `0${timeObj.getSeconds()}` : `${timeObj.getSeconds()}`;
+                let timeString = `${hours}:${mins}:${secs}`
+                
+                this.chartLabels.push(timeString);
+                elevationDataset.data.push(this.tour.content[i + 1].attributes.ele);
+                energyDataset.data.push(e);
+                speedDataset.data.push(v);
             }
         }
+
+        this.chartDatasets.push(energyDataset);
+        this.chartDatasets.push(elevationDataset);
+        this.chartDatasets.push(speedDataset);
 
         return kcal;
     }
@@ -53,7 +81,7 @@ class GPXHikeAnalysis
     /* d = distance [meters], Δh = height difference [meters], returns gradient [%]*/
     static gradient = (d, Δh) =>
     {
-        console.log(`Slope is ${((Math.atan(Δh / d) * (180 / Math.PI)).toFixed(2))}°`)
+        // console.log(`Slope is ${((Math.atan(Δh / d) * (180 / Math.PI)).toFixed(2))}°`)
         return 100 * (Δh / d);
     }
 
@@ -69,5 +97,52 @@ class GPXHikeAnalysis
         return 1.44 + 1.94 * Math.pow(v, 0.43) + 
                       0.24 * Math.pow(v, 4) +
                       0.34 * v * i * (1 - Math.pow(1.05, 1 - Math.pow(1.11, i + 32)));
+    }
+
+    makeChart = (canvas) =>
+    {
+        new Chart(canvas, 
+        {
+            type: 'line',
+            data:
+            {
+                labels: this.chartLabels,
+                datasets: this.chartDatasets
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    xAxes: [{
+                        afterTickToLabelConversion: function (data) {
+
+                            var xLabels = data.ticks;
+
+                            xLabels.forEach(function (labels, i) {
+                                if (i % 2 == 1) {
+                                    xLabels[i] = '';
+                                }
+                            });
+                        }
+                    }]   
+                }
+            }
+        });
+    }
+}
+
+class ChartDataset
+{
+    constructor(label, backgroundColor, borderColor, borderWidth = 1, pointRadius = 0)
+    {
+        this.label = label;
+        this.data = [];
+        this.backgroundColor = [backgroundColor];
+        this.borderColor = [borderColor];
+        this.borderWidth = borderWidth;
+        this.pointRadius = pointRadius;
     }
 }
--- sketch.js
+++ sketch.js
@@ -2,42 +2,25 @@ let G;
 let R;
 let RA;
 let bounds;
+let gui;
 
 const sketch = p =>
 {
-  let canvas;
   let trackBuf;
-  let graphBuf;
 
   p.setup = () =>
   {
-    G = GPXConverter.parse(testGpxString2);
-    R = G.routes;
-    console.log(R);
-    
-    RA = new GPXHikeAnalysis(80, 10, R);
-    console.log(RA.analyze());
-    
-    bounds = Object.assign(R.getExtrema('ele'), R.getExtrema('time'), R.getExtrema('lat'), R.getExtrema('lon'));
-    
+    p.createCanvas(window.innerWidth, window.innerHeight * 1/2);
 
-    p.createCanvas(window.innerWidth, window.innerHeight);
-
-    trackBuf = p.createGraphics(window.innerWidth, window.innerHeight * (3/4));
-    graphBuf = p.createGraphics(window.innerWidth, window.innerHeight * (1/4));
+    trackBuf = p.createGraphics(window.innerWidth, window.innerHeight * 1/2);
 
     p.background(53);
   }
 
   p.draw = () =>
   {
-    /* Draw buffers */
     plotTracks();
-    plotGraphs();
-
-    /* Plot buffers to canvas */
     p.image(trackBuf, 0, 0);
-    p.image(graphBuf, 0, trackBuf.height);
 
   }
 
@@ -70,52 +53,44 @@ const sketch = p =>
     });
     trackBuf.endShape();
   }
-
-  const plotGraphs = () =>
-  {
-    graphBuf.background(220);
-
-    graphBuf.stroke(200,0,0);
-    graphBuf.strokeWeight(1);
-    graphBuf.noFill();
-
-    graphBuf.beginShape();
-    R.content.forEach((rtept, i) => 
-    {
-      if (rtept.attributes.hasOwnProperty('ele') && rtept.attributes.hasOwnProperty('time'))
-      {
-        let x = graphBuf.map(i, 0, R.content.length  , 0, graphBuf.width);
-        // x = graphBuf.map(rtept.attributes.time.getTime(), bounds.minTime.getTime(), bounds.maxTime.getTime(), 0, graphBuf.width);
-        let y = graphBuf.map(rtept.attributes.ele, bounds.maxEle, bounds.minEle, 0, graphBuf.height);
-
-        graphBuf.vertex(x, y);
-      }
-    });
-    graphBuf.endShape();
-
-    /* Plot crosshair */
-    graphBuf.stroke(100);
-    graphBuf.strokeWeight(1);
-
-    if (p.mouseY >= p.height - graphBuf.height)
-    {
-      graphBuf.line(p.mouseX, 0, p.mouseX, graphBuf.height);
-
-      let i = parseInt(graphBuf.map(p.mouseX, 0, graphBuf.width, 0, R.content.length), 10);
-      let pt = R.content[i];
-     
-      let canvasSize = trackBuf.width > trackBuf.height ? trackBuf.height : trackBuf.width;
-      
-      let x = trackBuf.map(pt.attributes.lon, bounds.minLon, bounds.maxLon, 0, canvasSize);
-      let y = trackBuf.map(pt.attributes.lat, bounds.minLat, bounds.maxLat, canvasSize, 0);
-
-      trackBuf.fill(100);
-      trackBuf.ellipse(x, y, 10);
-    }
-  }
 }
 
-new p5(sketch);
 
+
+
+
+let doAnalyze = () =>
+{
+  let calories = RA.analyze();
+  console.log(calories);
+  RA.makeChart(chartCanv);
+}
+
+let gpxDiv = document.getElementById('gpx');
+let chartCanv = document.getElementById('chart').getContext('2d');
+
+gpxDiv.width = window.innerWidth;
+gpxDiv.height = window.innerHeight * 1/2;
+chartCanv.width = window.innerWidth;
+chartCanv.height = window.innerHeight * 1/2;  
+
+G = GPXConverter.parse(testGpxString2);
+R = G.routes;
+console.log(R);
+
+bounds = Object.assign(R.getExtrema('ele'), R.getExtrema('time'), R.getExtrema('lat'), R.getExtrema('lon'));
+
+RA = new GPXHikeAnalysis(80, 10, R);
+doAnalyze();
+
+new p5(sketch, 'gpx');
+
+gui = new dat.GUI();
+
+let hwController = gui.add(RA, 'hikerWeight', 30, 150, 0.5);
+let awController = gui.add(RA, 'additionalWeight', 0, 100, 0.5);
+
+hwController.onFinishChange(() => { doAnalyze(); });
+awController.onFinishChange(() => { doAnalyze(); });
 
 
